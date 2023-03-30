b52986b99401358282aa565697dc370b720c74da
WD-Rafiul <rafiulislam331@gmail.com>
2023-03-30 17:44:48 +0600

    rembg virsion_1

---
+++ main.py
@@ -0,0 +1,10 @@
+from rembg import remove
+in_image = input("Enter a picture: ")
+out_image = input("Enter new name of that picture: ")
+with open(in_image, "rb") as i:
+    with open(out_image, "wb") as o:
+        input = i.read()
+        output = remove(input)
+        o.write(output)
+
+print("Thes image backgraund removed 😊😊")
